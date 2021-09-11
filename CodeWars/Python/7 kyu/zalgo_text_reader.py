875613ac017a4ec4befd7c1b7cf9138a3d5f34b8
geodimitrov <geo.dimitrov@yahoo.com>
2021-09-11 21:54:12 +0300

    Added solution

---
+++ CodeWars/Python/7 kyu/zalgo_text_reader.py
@@ -0,0 +1,8 @@
+# Convert zalgo text to ordinary string
+
+def read_zalgo(zalgotext):
+    # check every character if it's ascii, join the result to form an ordinary string
+    return ''.join(char for char in zalgotext if ord(char) in range(128))
+
+
+print(read_zalgo("H̗̪͇͓̙͎̣̄ͬa͚̯̦͉̖̥v͆ͩ̃͆̓̐ͥe̟͎͖͕͍̎ ̰͚̩̟͕̰͊̽̍ͯ̌͊ā̖̪͉͍̥͙̿ͩ̃ͅ ̬̥͎͑̿ͧg̰̳̺͔̦͉ͫ̀̐̓̐r̝̫̱̘̰͐͋ͯͭͭͭ͆e͙͕̖̗͙̰͌ͭä͓͚̝͓́̌͑ͪ͊ṱͥ ̱ͣd͎͔͎͇̫̪̘̃͐̇à͕̮̈͋ͪy̼̳̱ͮ!̳̥̰̭͇̔ͮ̽̓")) # "Have a great day!")
