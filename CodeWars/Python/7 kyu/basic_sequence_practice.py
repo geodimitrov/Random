016733acb98e4a07b1096fce573bdabfbf32f343
geodimitrov <geo.dimitrov@yahoo.com>
2021-12-30 19:47:33 +0200

    added 7 kyu solutions

---
+++ CodeWars/Python/7 kyu/basic_sequence_practice.py
@@ -0,0 +1,7 @@
+def sum_of_n(n):
+    # triangular number formula: n(n + 1)/2
+    result = [i * (i + 1) / 2 for i in range(abs(n) + 1)]
+
+    if n < 0:
+        return [-el for el in result]
+
---
+++ CodeWars/Python/7 kyu/chicken_sexing.py
@@ -0,0 +1,9 @@
+def correctness(bobs_decisions, expert_decisions): 
+    score = 0
+
+    for i in range(len(bobs_decisions)):
+        if bobs_decisions[i] == expert_decisions[i]:
+            score += 1
+        elif bobs_decisions[i] == '?' or expert_decisions[i] == '?':
+            score += 0.5
+
